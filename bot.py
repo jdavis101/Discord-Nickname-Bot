b18ad5d1a04937df414e441b00dcd80460c87e33
JxChillin <Jaylend10@gmail.com>
2023-04-27 16:21:49 -0400

    Currently prints the bots name

--- bot.py
+++ bot.py
@@ -1,18 +1,26 @@
 import discord
+from discord.ext import commands
+from discord.ext.commands import bot
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents = intents)
+client = discord.Client(intents=intents)
+
+
 
 @client.event
 async def on_ready():
   print(f'We have logged in as {client.user}')
 
+
 @client.event
 async def on_message(message):
-      if message.author == client.user:
-        return
+  if message.author == client.user:
+    return
 
-      if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
+
+  if message.content.startswith('$what is my name'):
+    await message.channel.send(f'Hello! {client.user}' )
