bda24db0076f951bdc4c0f9d90d9f05802f06e19
JxChillin <Jaylend10@gmail.com>
2023-04-28 14:49:35 -0400

    Can print bot and message author name currently

--- bot.py
+++ bot.py
@@ -18,9 +18,24 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-
+  # bse messsage response
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
 
-  if message.content.startswith('$what is my name'):
-    await message.channel.send(f'Hello! {message.author}' )
+  # Goal: to change uthor display name
+  # currently prints display name to console
+  # why does it print twice?
+  if message.content.startswith('$r'):
+    await message.channel.send(f'Hello! {message.author}')
+    # await message.channel.send(f'test:  {message.guild.members.username }')
+    for guild in client.guilds:
+        for member in guild.members:
+            # print(member.display_name)
+            # prints the display name of the author and
+            # the person who sent the message
+            print((f'Name is {member.display_name}'))
+            print((f'Name is {message.author.display_name}'))
+            break
+        
+        # Key left out for security reasons
+        client.run('apikey')
