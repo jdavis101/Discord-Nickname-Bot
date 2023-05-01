7347c504fb3e06b4645085ba3f375d64defaf1e9
JxChillin <jaylend10@gmail.com>
2023-04-30 22:08:59 -0400

    updated member edit call, 403 error

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
+num = 1
 
 
 @client.event
@@ -18,28 +18,34 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-  # bse messsage response
+
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
 
-  # Goal: to change uthor display name
-  # currently prints display name to console
-  # why does it print twice?
   if message.content.startswith('$r'):
+
     #await message.channel.send(f'Hello! {message.author}')
     # await message.channel.send(f'test:  {message.guild.members.username }')
+    # how tf do I change the nickname with this??
     for guild in client.guilds:
-       for member in guild.members:
-            # print(member.display_name)
-            # print((f'Name is {member.display_name}'))
-            await message.channel.send(f'Hello {message.author.display_name}')
-            text = message.content
-            ntext = text[3:]
-            await message.channel.send(f'You said: {ntext}')
-            # await message.author.display_name.edit(nick="pig benis")
-            await member.edit(nick="ntext")
-            
-            break
-        
+      for member in guild.members:
+        # print(member.display_name)
+        # print((f'Name is {member.display_name}'))
+        Nmember = message.author.display_name
+        await message.channel.send(f'Hello {Nmember}')
+        text = message.content
+        ntext = text[3:]
+        await member.edit(nick = ntext )
+        #await message.channel.send(f'{member.nick} said: {ntext}')
+        # await message.author.display_name.edit(nick="pig benis")
+        #await member.edit(nick="ntext")
+
+        break
+
+# @bot.command()
+# async def name(ctx):
+#     pass
+#   member.edit(nick = ctx )
+
         # Key left out for security reasons
         client.run('apikey')
