f3922a8b0106ec08b023dcd4022adbc786a50010
JxChillin <jaylend10@gmail.com>
2023-05-02 20:31:31 -0400

    added comment for discord use

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import discord
 from discord.ext import commands
 
-
+#figure out how to get bot as a "/" command
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
  
 
 @bot.command()
