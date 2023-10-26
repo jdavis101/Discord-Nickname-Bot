d7290dbad678ea9b05057d22f2621e72d71a2b4c
JxChillin <Jaylend10@gmail.com>
2023-10-26 10:11:47 -0400

    Added key to nonshared folder

--- bot.py
+++ bot.py
@@ -1,18 +1,20 @@
+import os
 import discord
 from discord.ext import commands
 
-#figure out how to get bot as a "/" command
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='$', intents=intents)
- 
+bot = commands.Bot(command_prefix='!', intents=intents)
+BotToken = os.environ['Secret_Key']
 
 @bot.command()
 async def cn(ctx, member: discord.Member, *, new_nick):
+    """Change a member's nickname."""
     await member.edit(nick=new_nick)
     await ctx.send(f"Changed {member.mention}'s nickname to {new_nick}.")
 
-# Key left out for security reasons
-bot.run('apikey')
+
+bot.run(BotToken)
