ab147c2930a1c1c56ccb04a8393813810fb37c82
JxChillin <Jaylend10@gmail.com>
2023-10-26 10:16:43 -0400

    updated file structure

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@ import os
 import discord
 from discord.ext import commands
 
-
+path = os.environ["Secret_Key"]
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
-BotToken = os.environ['Secret_Key']
+#if used in replit create in secret tab
+#BotToken = os.environ['Secret_Key']
 
 @bot.command()
 async def cn(ctx, member: discord.Member, *, new_nick):
@@ -17,4 +18,4 @@ async def cn(ctx, member: discord.Member, *, new_nick):
     await ctx.send(f"Changed {member.mention}'s nickname to {new_nick}.")
 
 
-bot.run(BotToken)
+bot.run(path)
